33d47af01e2e8919cd356c569f160ed1b36e2906
sanjana <sanjanakiei20@gmail.com>
2025-06-15 14:03:08 +0530

    change on file

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use("/api/tickets", require("./Routes/ticket"));
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(` Server running on port:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
