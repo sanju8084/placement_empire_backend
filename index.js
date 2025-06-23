66085a8f434022676484095cf8fba2f86c7b5628
sanjana <sanjanakiei20@gmail.com>
2025-06-23 13:40:19 +0530

    update

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api/tickets", require("./routes/ticket"));
-app.use("/api/admin", require("./routes/admin"));
+app.use("/api/tickets", require("./Routes/ticket"));
+app.use("/api/admin", require("./Routes/admin"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
